6763e4c82cf54102b172f54132e521984c6df14f
LuanNguyen <kesanbauvat9@gmail.com>
2020-08-18 15:54:52 +0700

    83

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,16 +5,31 @@ const URL = 'http://navilearn.herokuapp.com/';
 //     <a href="${URL}/quen-mat-khau/${code}">
 //     ${URL}/quen-mat-khau/${code}</a>
 //  `;
-
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const oauth2Client = new OAuth2(
+    "465186508352-jmpr1f8buetcoheom017g2dctsroa5pp.apps.googleusercontent.com",
+    "x8ATYu5cwHnoOofEUrnte1Xu",
+    "https://developers.google.com/oauthplayground"
+);
+oauth2Client.setCredentials({
+    refresh_token: "1//0fkeAOT3NCSrKCgYIARAAGA8SNwF-L9IrPZr5XG9kDCVplc6Lw7DhUC2c1wSse5Yl6RpPhJ-xZhI7XNcK2Ss_6oxscHVtDzZ1G_4"
+});
+const accessToken = oauth2Client.getAccessToken()
 const createMailOption = { // config mail server
     service:'gmail',
-    host: 'smtp.googlemail.com',
-    port: 587,
-    secure:true,
-    requireTLS: false,
+    // host: 'smtp.googlemail.com',
+    // port: 587,
+    // secure:true,
+    // requireTLS: false,
     auth: {
+        type: 'OAuth2',
         user: 'hieuluan.2109@gmail.com',
-        pass: 'Cookcie00999'
+        // pass: 'Cookcie00999',
+        clientId: '465186508352-jmpr1f8buetcoheom017g2dctsroa5pp.apps.googleusercontent.com',
+        clientSecret: 'x8ATYu5cwHnoOofEUrnte1Xu',
+        refreshToken: "1//0fkeAOT3NCSrKCgYIARAAGA8SNwF-L9IrPZr5XG9kDCVplc6Lw7DhUC2c1wSse5Yl6RpPhJ-xZhI7XNcK2Ss_6oxscHVtDzZ1G_4",
+        accessToken: accessToken,
     },
     tls: {
         // do not fail on invalid certs
